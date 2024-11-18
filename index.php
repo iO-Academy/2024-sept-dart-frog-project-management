59bf73080f0f73d4ec8d94292fdc264efedbcd70
Charlie Bird <cbvvrd@gmail.com>
2024-11-18 16:52:46 +0000

    added entity & model for Client

--- index.php
+++ index.php
@@ -2,12 +2,19 @@
 
 require_once 'src/Services/DatabaseService.php';
 require_once 'src/Models/ProjectsModel.php';
+require_once 'src/Models/ClientsModel.php';
 
 $db = DatabaseService::connect();
 
-$projectModel = new ProjectsModel($db);
+//$projectModel = new ProjectsModel($db);
+//$test = $projectModel->selectProject();
+//echo '<pre>';
+//var_dump($test);
 
-$test = $projectModel->selectProject();
+$clientsModel = new ClientsModel($db);
+$allClients = $clientsModel->getAllClients();
+$singleClient = $clientsModel->getClientById(13);
 
 echo '<pre>';
-var_dump($test);
+var_dump($singleClient);
+var_dump($allClients);
