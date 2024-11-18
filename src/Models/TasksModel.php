714ace7c971ae81d646bc8186c35f5721890b8a4
Ben Warr <warrbenjamin@gmail.com>
2024-11-18 16:57:53 +0000

    task model updated

--- src/Models/TasksModel.php
+++ src/Models/TasksModel.php
@@ -1,2 +1,21 @@
 <?php
 
+require_once 'src/Services/DatabaseService.php';
+
+class TasksModel
+{
+
+    private PDO $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function selectTask()
+    {
+        $query = $this->db->prepare('SELECT * FROM `tasks`;');
+        $query->execute();
+        return $query->fetchAll();
+    }
+}
