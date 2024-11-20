391ecc21cdac63b8e1d7dcd5a65b5c0f03ab2003
Oksana Bakhaieva <obahaeva@gmail.com>
2024-11-20 01:57:17 +0000

    Changed date format displayed in task

--- task.php
+++ task.php
@@ -23,23 +23,26 @@ require_once 'src/Entities/TaskEntity.php';
 
 $db = DatabaseService::connect();
 $tasksModel = new TasksModel($db);
-$id = 300;
+$taskId = 3;
 
-$displayTask = $tasksModel->selectTaskById($id);
+$displayTask = $tasksModel->selectTaskById($taskId);
 $displayTaskName = $displayTask->name;
 $displayTaskDescription = $displayTask->description;
 $displayTaskEstimate = $displayTask->estimate;
-$displayTaskDeadline = $displayTask->deadline;
 
-$displayTaskUser = $tasksModel->displayTaskUser($id);
+$displayTaskDeadline = $displayTask->deadline;
+$date = new DateTimeImmutable($displayTaskDeadline);
+$dateNewFormat = $date->format('d/m/y');
+
+$displayTaskUser = $tasksModel->displayTaskUser($taskId);
 $displayTaskUserName = $displayTaskUser->name;
 $displayTaskUserAvatar = $displayTaskUser->avatar;
-//echo $displayTaskUserAvatar;
+
 ?>
 
     <div class="flex justify-between mb-3">
-        <h2 class="text-4xl font-bold mb-2"><?php echo $displayTaskName . '-' .
-                $displayTaskDeadline?>
+        <h2 class="text-4xl font-bold mb-2"><?php echo $displayTaskName . ' - ' .
+                $dateNewFormat?>
             <a href="project.php" class="text-base text-blue-600 hover:underline ms-3">Return to project</a>
         </h2>
         <div class="flex items-center gap-3">
@@ -54,7 +57,7 @@ $displayTaskUserAvatar = $displayTaskUser->avatar;
         </div>
         <div class="w-1/2">
             <h5 class="text-lg font-bold">Task Deadline:</h5>
-            <p class="text-red-500"><?php echo $displayTaskDeadline?></p>
+            <p class="text-red-500"><?php echo $dateNewFormat?></p>
         </div>
         <div class="w-full my-3">
             <h5 class="text-lg font-bold">Task Description:</h5>
