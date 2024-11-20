41f35a4e537eb66bf73dd8e89ff8443c6b1982da
Ian Blenkinsop <blenka2@gmail.com>
2024-11-20 11:57:57 +0000

    date and overdue formatting fixed in tasks

--- task.php
+++ task.php
@@ -23,7 +23,7 @@ require_once 'src/Entities/TaskEntity.php';
 
 $db = DatabaseService::connect();
 $tasksModel = new TasksModel($db);
-$taskId = 2;
+$taskId = 3;
 
 $displayTask = $tasksModel->selectTaskById($taskId);
 $displayTaskName = $displayTask->name;
@@ -44,6 +44,14 @@ function checkDateValid($dateinput){
 
 $dateNewFormat = checkDateValid($displayTaskDeadline);
 
+function isOverdue($date){
+    $today = date('Y-m-d');
+    if ($today > $date) {
+    return true;
+} else return false;
+}
+
+
 $displayTaskUser = $tasksModel->displayTaskUser($taskId);
 $displayTaskUserName = $displayTaskUser->name;
 $displayTaskUserAvatar = $displayTaskUser->avatar;
@@ -51,8 +59,9 @@ $displayTaskUserAvatar = $displayTaskUser->avatar;
 ?>
 
     <div class="flex justify-between mb-3">
-        <h2 class="text-4xl font-bold mb-2"><?php echo $displayTaskName . ' - ' .
-                $dateNewFormat?>
+        <h2 class="text-4xl font-bold mb-2"><?php echo $displayTaskName . ' - ' . ?>
+
+
             <a href="project.php" class="text-base text-blue-600 hover:underline ms-3">Return to project</a>
         </h2>
         <div class="flex items-center gap-3">
@@ -67,7 +76,13 @@ $displayTaskUserAvatar = $displayTaskUser->avatar;
         </div>
         <div class="w-1/2">
             <h5 class="text-lg font-bold">Task Deadline:</h5>
-            <p class="text-red-500"><?php echo $dateNewFormat?></p>
+            <?php  if (isOverdue($displayTaskDeadline)){
+                echo "<p class=\"text-red-500\">$dateNewFormat</p>";
+            } else {
+                echo "<p class=\"text-black\">$dateNewFormat</p>";
+            }
+            ?>
+
         </div>
         <div class="w-full my-3">
             <h5 class="text-lg font-bold">Task Description:</h5>
